f9e41d79516b77bb69ca2bfb334306a0405b6878
YethHound <jerzy.kaplinski@gmail.com>
2017-10-10 15:31:33 +0300

    Message 2

--- src/main/java/decacalc/PointCalculator.java
+++ src/main/java/decacalc/PointCalculator.java
@@ -7,14 +7,14 @@ public class PointCalculator{
 	private static HashMap<String, Event> events = new HashMap<>();
 	
 	private static Event hundredMeterDash = new Event("100m",true,25.4347,18,1.81);
-	private static Event longJump = new Event("Long jump",false,0.14354,220,1.4);
-	private static Event shotPut = new Event("Shot put",false,51.39,1.5,1.05);
-	private static Event highJump = new Event("High jump",false,0.8465,75,1.42);
+	private static Event longJump = new Event("Long_jump",false,0.14354,220,1.4);
+	private static Event shotPut = new Event("Shot_put",false,51.39,1.5,1.05);
+	private static Event highJump = new Event("High_jump",false,0.8465,75,1.42);
 	private static Event fourHundredMeterDash = new Event("400m",true,1.53775,82,1.81);
-	private static Event hurdles = new Event("110m hurdles",true,5.74352,28.5,1.52);
-	private static Event discus = new Event("Discus throw",false,12.91,4,1.1);
-	private static Event poleVault = new Event("Pole vault",false,0.2797,100,1.35);
-	private static Event javelin = new Event("Javelin throw",false,10.14,7,1.08);
+	private static Event hurdles = new Event("110m_hurdles",true,5.74352,28.5,1.52);
+	private static Event discus = new Event("Discus_throw",false,12.91,4,1.1);
+	private static Event poleVault = new Event("Pole_vault",false,0.2797,100,1.35);
+	private static Event javelin = new Event("Javelin_throw",false,10.14,7,1.08);
 	private static Event thousandFiveHundredMeterRun = new Event("1500m",true,0.03768,480,1.85);
 	
 	private static void populateEvents() {
@@ -36,7 +36,7 @@ public class PointCalculator{
 		populateEvents();
 
 		if(events.get(eventName)==null){
-			throw new IllegalArgumentException("Valid parameters are: '100m', 'long jump','shot put','high jump','400m','110m hurdles','discus throw','pole vault','javelin throw' and '1500m'");
+			throw new IllegalArgumentException("Valid parameters are: '100m', 'Long_jump','Shot_put','High_jump','400m','110m_hurdles','Discus_throw','Pole_vault','Javelin_throw' and '1500m'");
 		}
 		
 		Event event = events.get(eventName);
